434f77cb370584bd3f301a01624d40318aed5e71
HynekS <hyneksvacha@seznam.cz>
2021-11-16 19:52:31 +0100

    Add 'internalLink' mark to BlockContent schema

--- schemas/blockContent.js
+++ schemas/blockContent.js
@@ -57,6 +57,22 @@ export default {
               },
             ],
           },
+          {
+            name: "internalLink",
+            type: "object",
+            title: "Internal link",
+            fields: [
+              {
+                name: "reference",
+                type: "reference",
+                title: "Reference",
+                to: [
+                  { type: "post" },
+                  // other types you may want to link to
+                ],
+              },
+            ],
+          },
         ],
       },
     },
@@ -90,7 +106,7 @@ export default {
     {
       type: "blockQuote",
     },
-	{
+    {
       type: "gallery",
     },
   ],
